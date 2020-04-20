89cae9be956b55124e9a1dbdca101bc8254b65e2
davidroman-hub <jobroman83@gmail.com>
2020-04-20 14:32:36 +0100

    Adding npm packages and putting in our app

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+
 const app = express()
 
 // IMPORT ROUTES
 
 const authRoutes = require('./routes/auth')
+
+//App middelwares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
 
 
 // middleware
